c25f8b53e610fc8c76645d959b9638e76a353a18
Ryan Bahan <ryan@ryanbahan.com>
2019-11-25 23:53:46 -0700

    Add animation to welcome banner close

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 (function hideBanner() {
   const welcomeBanner = document.getElementById('welcome-banner')
   const bannerCloseBtn = document.getElementById('banner-close-btn')
+  const contentColumn = document.getElementById('primary-content')
 
-  bannerCloseBtn.addEventListener('click', hide)
+  bannerCloseBtn.addEventListener('click', fadeOut)
 
-  function hide() {
-    welcomeBanner.classList.add('hide');
+  function fadeOut() {
+    welcomeBanner.classList.add('fade-out');
+    setTimeout(hide, 500);
+    function hide() {
+      welcomeBanner.classList.add('hide');
+    }
   }
 })();
